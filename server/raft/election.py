2ec1f03efc01d545b1bba50cbd9d25b85afe55dc
Rahul Chunduru <rahulchunduru@Rahuls-MBP.lan>
2023-04-05 00:11:20 -0500

    fix new leader election

--- server/raft/election.py
+++ server/raft/election.py
@@ -103,7 +103,7 @@ class Election:
                     self.trigger_election()
 
         # Got majority votes: become leader
-        if votes_received > num_peers // 2:
+        if votes_received >= num_peers // 2:
             globals.state = NodeRole.Leader
             log_me(f"{globals.name} became leader!")
             globals.leader_name = globals.name
