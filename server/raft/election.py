557a29528136b42c48d1683e05b75e4781e6da6d
Tabish <taabishm2@gmail.com>
2023-04-05 11:42:47 -0500

    Add delay to init_timeout if IS_UNRESPONSIVE configured

--- server/raft/election.py
+++ server/raft/election.py
@@ -1,4 +1,5 @@
 import time
+from os import environ
 from threading import Thread
 
 from .config import NodeRole, globals
@@ -66,7 +67,9 @@ class Election:
 
     def init_timeout(self):
         """Checks for missed heartbeats from the leader and start the election"""
-
+        if environ['IS_UNRESPONSIVE'] == "True":
+            log_me("Node was configured to be UNRESPONSIVE: waiting 5 secs")
+            time.sleep(5)
         try:
             rand_timeout = random_timeout(globals.LOW_TIMEOUT, globals.HIGH_TIMEOUT)
             globals.curr_rand_election_timeout = time.time() + rand_timeout
--- server/raft/node.py
+++ server/raft/node.py
@@ -11,10 +11,10 @@ class RaftNode:
 
     def serve_put_request(self, key, value):
         """Returns tuple: (success (bool), error message) TODO: Redirect to leader node."""
-        if globals.is_unresponsive:
-            log_me("Am going to sleepzzzz")
-            while True:
-                sleep(1)
+        # if globals.is_unresponsive:
+        #     log_me("Am going to sleepzzzz")
+        #     while True:
+        #         sleep(1)
 
         log_item = LogEntry(globals.current_term, key, value)
         index = log_manager.append(log_item)
